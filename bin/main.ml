8821b68e677072bd03cddbf8860aec298fb470ee
sravan <sravan-s@protonmail.com>
2024-02-28 17:05:47 +0900

    simple post request

--- bin/main.ml
+++ bin/main.ml
@@ -1,4 +1,37 @@
-let add5 x = x + 5
+open Ppx_yojson_conv_lib.Yojson_conv.Primitives
 
-let () = print_endline @@ string_of_int @@ add5 10
-let () = print_endline(string_of_int(add5 10))
+type message_object = {
+  message : string;
+} [@@deriving yojson]
+
+(* www.wikipedia.com/a/b/c => Link.lasso/v *)
+
+let () =
+  Dream.run
+  @@ Dream.logger
+  @@ Dream.router [
+
+    Dream.get "/"
+      (fun _ ->
+        Dream.html "Good morning, world!");
+
+    Dream.post "/"
+      (fun request ->
+        let%lwt body = Dream.body request in
+
+        let message_object =
+          body
+          |> Yojson.Safe.from_string
+          |> message_object_of_yojson
+        in
+
+        `String message_object.message
+        |> Yojson.Safe.to_string
+        |> Dream.json);
+
+    Dream.get "/echo/:word"
+      (fun request ->
+        Dream.html ((Dream.param request "word") ^ " , hola"));
+
+  ]
+
